e84e79520a0e5ca859c3f7bb6385e1fe91b9b6e6
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-15 21:23:13 +0300

    add spinner to show loading state

--- src/components/search-result/search-result.js
+++ src/components/search-result/search-result.js
@@ -6,20 +6,23 @@ class SearchResult {
     const searchFormRoot = document.querySelector('.js-search-form');
     this.searchForm = new SearchForm(searchFormRoot);
     this.searchForm.addObserver(this);
+    this.spinner = document.querySelector('.js-search-result__loader');
+    this.searchWrapper = document.querySelector('.js-search-result');
+    this.searchList = document.querySelector('.js-search-result__list');
+    this.curtain = document.querySelector('.js-search-result__curtain');
   }
 
   async handleEvent(message, data) {
     if (message === 'search') {
+      this.curtain.classList.remove('search-result__curtain_disabled');
+      this.spinner.classList.remove('search-result__loader_disabled');
       const queryString = `q=${encodeURIComponent(`${data} in:name`)}`;
       const request = await fetch(`https://api.github.com/search/repositories?${queryString}`);
       const result = await request.json();
-      const fragment = document.createDocumentFragment();
-      const ul = document.createElement('ul');
-      ul.classList.add('search-result__list');
       if (result.total_count === 0) {
-        ul.textContent = 'Ничего не найдено';
+        this.searchList.innerHTML = '<li><span>Ничего не найдено</span></li>';
       } else {
-        this.searchResultRoot.textContent = '';
+        this.searchList.innerHTML = '';
         const MAX_ELEMS_TO_SHOW = 10;
         const maxOfElements = result.total_count >= MAX_ELEMS_TO_SHOW
           ? MAX_ELEMS_TO_SHOW : result.total_count;
@@ -36,11 +39,11 @@ class SearchResult {
           span.textContent = `Описание: ${result.items[i].description}`;
           li.appendChild(a);
           li.appendChild(span);
-          ul.appendChild(li);
+          this.searchList.appendChild(li);
         }
       }
-      fragment.appendChild(ul);
-      this.searchResultRoot.appendChild(fragment);
+      this.curtain.classList.add('search-result__curtain_disabled');
+      this.spinner.classList.add('search-result__loader_disabled');
     }
   }
 }
